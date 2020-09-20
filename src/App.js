fbe5e7fbd150f96f8a11a4f55c617b5d8c19e064
Mariola Pasko <paskomariola@gmail.com>
2020-09-21 00:34:42 +0200

    map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Map from './Components/Map'
+import Map from './Components/Map';
 
 function App() {
   return (
--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,19 +1,25 @@
 import React from "React";
-import ReactMapGL from 'react-map-gl'
+import ReactMapGL from "react-map-gl";
 
 const viewport = {
-    latitute:,
-    longitude:,
+    latitute: 51.5049375,
+    longitude: -0.0964509,
     zoom: 13
 }
-return (
-    <ReactMapGL
-        width="100vw"
-        height="100vw"
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-        mapboxApiAccessToken="pk.eyJ1IjoibW9raXBhIiwiYSI6ImNrZmJuN3N3YTE3bGoycXFncnFhNjlwYzUifQ.wil8qLji6sWKhdUSNGVnKg"
-    >
 
-    </ReactMapGL>
-)
+const Map = ({ classes }) => {
+    return (<div>
+        <ReactMapGL
+            width="100vw"
+            height="100vw"
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+            mapboxApiAccessToken="pk.eyJ1IjoibW9raXBhIiwiYSI6ImNrZmJuN3N3YTE3bGoycXFncnFhNjlwYzUifQ.wil8qLji6sWKhdUSNGVnKg"
+            {...viewport}
+        
+        >
+
+        </ReactMapGL>
+    </div>);
+}
+
 export default Map; 
