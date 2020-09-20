6f0ac3c7f4947124dc3408d6cbab6ba768f4c26b
Mariola Pasko <paskomariola@gmail.com>
2020-09-21 00:12:20 +0200

    first commit

---
+++ src/Components/Map.js
@@ -0,0 +1,19 @@
+import React from "React";
+import ReactMapGL from 'react-map-gl'
+
+const viewport = {
+    latitute:,
+    longitude:,
+    zoom:
+}
+return (
+    <ReactMapGL
+        width="100vw"
+        height="100vw"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1IjoibW9raXBhIiwiYSI6ImNrZmJuN3N3YTE3bGoycXFncnFhNjlwYzUifQ.wil8qLji6sWKhdUSNGVnKg"
+    >
+
+    </ReactMapGL>
+)
+export default Map; 
