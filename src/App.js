c198fdc385169db6e511a5ad4c6f32647f3133dd
Mariola Pasko <paskomariola@gmail.com>
2020-09-21 23:16:02 +0200

    map

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
+import GoogleMapReact from "google-map-react";
 
 
 function App() {
   return (
-    <div className="Map App">
-      <header className="App-header">
-        <p>
-          Map
-        </p>
-      </header>
-      <Map />
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyAvX2jNZU - jywa6N7yUHTsP8W3N30orbOM" }}
+        defaultCenter={this.props.center}
+        defaultZoom={this.props.zoom}
+      ></GoogleMapReact>
     </div>
   );
 }
